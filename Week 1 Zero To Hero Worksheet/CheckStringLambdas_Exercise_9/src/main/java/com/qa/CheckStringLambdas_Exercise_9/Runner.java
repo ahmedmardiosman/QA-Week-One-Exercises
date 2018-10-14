fa99524c56c666f9b3652f6d7392588203cd3d9c
ahmedmardiosman <ahmedmardiosman@gmail.com>
2018-10-15 00:11:29 +0100

    Ninth Commit

--- Week 1 Zero To Hero Worksheet/CheckStringLambdas_Exercise_9/src/main/java/com/qa/CheckStringLambdas_Exercise_9/Runner.java
+++ Week 1 Zero To Hero Worksheet/CheckStringLambdas_Exercise_9/src/main/java/com/qa/CheckStringLambdas_Exercise_9/Runner.java
@@ -1,26 +1,22 @@
 package com.qa.CheckStringLambdas_Exercise_9;
 
-	public class Runner implements NameString {
+public class Runner implements NameString {
 
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 
-			String nameOne = "John";
+		String nameOne = "Ahmed";
 
-			String nameTwo = "Jouhn";
-			
-			NameString checker = (FirstString, SecondString) -> FirstString.equals(SecondString);
-			
-			System.out.println(checker.check(nameOne, nameTwo));
-			
+		String nameTwo = "Ahmed";
 
-		}
+		NameString checker = (FirstString, SecondString) -> FirstString.equals(SecondString);
 
-		public boolean check(String FirstString, String SecondString) {
-			return false;
-		}
-		
-		
+		System.out.println(checker.check(nameOne, nameTwo));
 
 	}
 
+	@Override
+	public boolean check(String FirstString, String SecondString) {
+		return false;
+	}
 
+}
